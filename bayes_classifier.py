4ad5de8d20fcd9ae4945071d629666c5f4c339b6
Grzegorz Legień <glegien@gmail.com>
2019-10-05 22:32:35 +0200

    Naive bayes

---
+++ bayes_classifier.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+
+class NaiveBayes(object):
+    def __init__(self, file_):
+        data_set = pd.read_csv(file_, index_col=0)
+        self.classifier = GaussianNB()
+        features = np.array(data_set[["AGE"]])
+        target = np.array(data_set["is_obesity"]).reshape(-1, 1).ravel()
+        self.classifier.fit(features, target)
+
+    def predict(self, value):
+        return self.classifier.predict(np.array([value]).reshape(1, 1))[0]
--- ml_server.py
+++ ml_server.py
@@ -1,17 +1,30 @@
 from flask import Flask, request
 from gevent.pywsgi import WSGIServer
 
+from bayes_classifier import NaiveBayes
+
 app = Flask(__name__)
+
+naive = {'male': NaiveBayes("male_data_0.csv"), 'female': NaiveBayes("female_data_0.csv")}
 
 
 @app.route('/predict', methods=['POST'])
 def hello():
+    print('Request {}'.format(request.values))
     sex = request.values.get('sex')
     model = request.values.get('model')
     if not model or not sex:
         raise Exception('Param undefined!')
-    print('Request {}'.format(request.values))
-    return 'OK'
+    if model == 'naive':
+        classifier = naive[sex]
+        age = request.values.get('age')
+        if not age:
+            raise Exception('Age param undefined!')
+        print 'Age: ' + age
+        prediction = str(classifier.predict(int(age)))
+        print('Result: ' + prediction)
+        return prediction
+    return 'Ignored...'
 
 
 if __name__ == '__main__':
